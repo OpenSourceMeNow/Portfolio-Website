a69a30d6b8f073f47cda4cc4a576efd2f51139af
OpenSourceMeNow <bryanmunera@hotmail.com>
2024-04-23 14:36:51 -0400

    link fix

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -10,15 +10,17 @@ interface Props {
 
 const ProjectCard = ({ src, title, description, link }: Props) => {
   return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
-      <a href={link} target="_blank" rel="noopener noreferrer">
-        <Image src={src} alt={title} width={1000} height={1000} className="w-full object-contain hover:scale-105 transition-transform duration-300" />
-      </a>
-      <div className="relative p-4">
-        <h1 className="text-2xl font-semibold text-white">{title}</h1>
-        <p className="mt-2 text-gray-300">{description}</p>
+    <a href={link} target="_blank" rel="noopener noreferrer" className="block">
+      <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
+        <div className="w-full object-contain hover:scale-105 transition-transform duration-300">
+          <Image src={src} alt={title} width={1000} height={1000} />
+        </div>
+        <div className="relative p-4">
+          <h1 className="text-2xl font-semibold text-white">{title}</h1>
+          <p className="mt-2 text-gray-300">{description}</p>
+        </div>
       </div>
-    </div>
+    </a>
   );
 };
 
