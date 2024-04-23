1e310a04b88ab32973bc4e11714946360824118e
OpenSourceMeNow <bryanmunera@hotmail.com>
2024-04-23 01:18:42 -0400

    deleted comments

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -21,22 +21,8 @@ const Encryption = () => {
         </motion.div>
       </div>
       <div className="flex flex-col items-center justify-center translate-y-[-50px] absolute z-[20] w-auto h-auto">
-        {/* <div className="flex flex-col items-center group cursor-pointer w-auto h-auto">
-          <Image
-            src="/LockTop.png"
-            alt="Lock top"
-            width={50}
-            height={50}
-            className="w-[50px] translate-y-5 transition-all duration-200 group-hover:translate-y-11"
-          />
-          <Image
-            src="/LockMain.png"
-            alt="Lock Main"
-            width={70}
-            height={70}
-            className=" z-10"
-          />
-        </div> */}
+        { <div className="flex flex-col items-center group cursor-pointer w-auto h-auto">
+        </div> }
       </div>
       <div className="absolute z-[20] bottom-[10px] px-[5px]">
         <div className="cursive text-[20px] font-medium text-center text-gray-300"></div>
