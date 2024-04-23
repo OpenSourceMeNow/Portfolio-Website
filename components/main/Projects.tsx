6600912ffed268ff24fa53a8592db07dff560aa8
OpenSourceMeNow <bryanmunera@hotmail.com>
2024-04-23 13:52:45 -0400

    added links to projects

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -3,10 +3,7 @@ import ProjectCard from "../sub/ProjectCard";
 
 const Projects = () => {
   return (
-    <div
-      className="flex flex-col items-center justify-center py-50"
-      id="projects"
-    >
+    <div className="flex flex-col items-center justify-center py-50" id="projects">
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         My Projects
       </h1>
@@ -15,16 +12,19 @@ const Projects = () => {
           src="/website.png"
           title="Modern Next.js Portfolio"
           description="Built an Interactive and speedy website to showcase my myself and my skills."
+          link="https://github.com/OpenSourceMeNow/Portfolio-Website"
         />
         <ProjectCard
           src="/catdog.jpeg"
           title="Image Classification Project"
           description="built an image classification model using tensorflow and keras to classify images of cats and dogs."
+          link="https://github.com/OpenSourceMeNow/Image-Classifier-Project-Cat-Dog"
         />
         <ProjectCard
           src="/stockprediction.png"
           title="Stock Prediction Model"
           description="Built a stock prediction model using machine learning and python to predict the stock prices of a company."
+          link="https://github.com/OpenSourceMeNow/Stock_Predictor"
         />
       </div>
     </div>
--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -1,26 +1,28 @@
 import Image from "next/image";
 import React from "react";
+import Link from "next/link";
 
 interface Props {
   src: string;
   title: string;
   description: string;
+  link?: string;
 }
 
-const ProjectCard = ({ src, title, description }: Props) => {
+const ProjectCard = ({ src, title, description, link }: Props) => {
   return (
     <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
-      <Image
-        src={src}
-        alt={title}
-        width={1000}
-        height={1000}
-        className="w-full object-contain"
-      />
-
+      <Image src={src} alt={title} width={1000} height={1000} className="w-full object-contain" />
       <div className="relative p-4">
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
         <p className="mt-2 text-gray-300">{description}</p>
+        {link && (
+          <Link href={link} passHref>
+            <a className="mt-4 inline-block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" target="_blank" rel="noopener noreferrer">
+              View Project
+            </a>
+          </Link>
+        )}
       </div>
     </div>
   );
